c05c208dd80da5bc321abe569855cb46551559ea
aneshprabus <aneshprabus@gmail.com>
2026-06-21 19:33:18 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,14 @@
 // js/supabase.js
+// ==========================================
+// Supabase Configuration
+// File: js/supabase.js
+// ==========================================
 
 const SUPABASE_URL = "https://jocuisqnrnkzzbontlqm.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_rvLs-6kiyS8lCcDejyn31w_3bjJj0fL";
-
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+console.log("✅ Supabase Connected");
